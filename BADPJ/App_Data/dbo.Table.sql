640f56d4a68d94e4f660818983acdc7ab8c0dc44
javier <javieryaukx@gmail.com>
2024-12-25 22:48:35 +0800

    ADDED PRODUCT DATABASE SUCCESFULLYY

---
+++ BADPJ/App_Data/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [UserName] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [UserPw] VARCHAR(255) NOT NULL, 
+    [UserType] CHAR(1) NOT NULL CHECK (UserType IN ('B', 'S'))
+);
---
+++ BADPJ/App_Data/dbo.Table2.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[ProductID] INT NOT NULL PRIMARY KEY, 
+    [ProductName] VARCHAR(50) NOT NULL, 
+    [ProductDesc] TEXT NULL, 
+    [Price] DECIMAL(10, 2) NOT NULL, 
+    [Colour] VARCHAR(100) NOT NULL, 
+    [SellerID] INT NULL
+)
